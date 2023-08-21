63517d78fb2bae673780f5b7f82240101d206532
Code021 <gabrielakinshola3@gmail.com>
2023-08-21 10:15:08 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,25 +7,23 @@
  */
 void rev_string(char *s)
 {
-	int len = 0;
-	int l = 0;
-	char *y = s;
-	int e = 0;
-	int x;
-	char n;
+	int n, i;
+	char *end_ptr, ch;
 
-	while (*y != '\0')
+	end_ptr = s;
+
+	for (n = 0 ; *end_ptr != '\0' ; n++)
 	{
-		y++;
-		len++;
+		end_ptr++;
 	}
-	l = len - 1;
-	for ( ; e < ((1 / 2) + 1) ; e++)
+	end_ptr = end_ptr - 1;
+
+	for (i = 0; i < n / 2 ; i++)
 	{
-		x = (1 - e);
-		n = s[e];
-		s[e] = s[x];
-		s[x] = n;
+		ch = *end_ptr;
+		*end_ptr = *(s + i);
+		*(s + i) = ch;
+		end_ptr--;
 	}
 }
 
