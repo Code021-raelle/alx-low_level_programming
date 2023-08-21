1b98123e7c69ce91ac6df6edf69c10e0fff44c13
Code021 <gabrielakinshola3@gmail.com>
2023-08-21 14:03:12 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - random password generation for 101-crackme
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
+
+	srand(time(NULL));
+	while (s != 2772)
+	{
+		j = rand() % 62;
+		p[i] = c[j];
+		s += c[j];
+		i++;
+	}
+	while (c[k])
+	{
+		if (c[k] == (2772 - s))
+		{
+			p[i] = c[k];
+			s += c[k];
+			i++;
+			break;
+		}
+		k++;
+	}
+	p[i] = '\0';
+	printf("%s", p);
+	return (0);
+}
+
