29f4270b1d649dc8405355ffb12480474f0a326d
Code021 <gabrielakinshola3@gmail.com>
2023-08-23 07:09:33 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13 cipher.
+ * @s: The input string.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *rot13(char *s)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13_alphabet[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKL
+		M";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (s[i] == alphabet[j])
+			{
+				s[i] = rot13_alphabet[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
+
