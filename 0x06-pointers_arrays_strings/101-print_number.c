55b6ccf9470e2574812dacffd8fd008d98ab6c75
Code021 <gabrielakinshola3@gmail.com>
2023-08-23 07:42:52 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer using _putchar.
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar(n % 10 + '0');
+}
+
