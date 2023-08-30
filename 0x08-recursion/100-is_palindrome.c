6c15ed5c7a1df054252943f34033d0e178021095
Code021 <gabrielakinshola3@gmail.com>
2023-08-30 07:52:20 -0700

    100-is_palidrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * pali - function to compare string.
+ * @i: counter to begin string.
+ * @len: length of the string.
+ * @s: string to evaluate.
+ *
+ * Return: 1 for palindrome, 0 for not palindrome.
+ */
+int pali(int i, int len, char *s)
+{
+	if (i == len - 1)
+	{
+		return (1);
+	}
+	else if (s[i] != s[len - 1])
+	{
+		return (0);
+	}
+	else if (i < len - 1)
+	{
+		return (pali(i + 1, len - 1, s));
+	}
+	return (1);
+}
+
+/**
+ * length - Function to calculate length of the string.
+ * @s: string to evaluate.
+ *
+ * Return: length of the string.
+ */
+int length(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + length(s + 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - Function to evaluate is a palindrome.
+ * @s: string to evaluate is a palindrome word.
+ *
+ * Return: 1 for palindrome, 0 for not palindrome.
+ */
+int is_palindrome(char *s)
+{
+	int len = length(s);
+
+	return (pali(0, len, s));
+}
+
