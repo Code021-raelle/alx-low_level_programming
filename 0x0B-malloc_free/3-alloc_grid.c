8f9e49f3c1fefc4ac909b926c93dc5819ace5881
Code021 <gabrielakinshola3@gmail.com>
2023-09-05 01:57:46 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,38 +10,30 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int i, j, k;
+	int **ptr;
+
 	if (width <= 0 || height <= 0)
-	{
 		return (NULL);
-	}
-
-	int **grid = (int **)malloc(height * sizeof(int *));
-
-	if (grid == NULL)
-	{
+	ptr = malloc(sizeof(int *) * height);
+	if (ptr == NULL)
 		return (NULL);
-	}
-
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(width * sizeof(int));
-
-		if (grid[i] == NULL)
+		ptr[i] = malloc(sizeof(int) * width);
+		if (ptr[i] == NULL)
 		{
-			for (int j = 0; j < i; j++)
-			{
-				free(grid[j]);
-			}
-			free(grid);
+			for (k = 0; k < i; k++)
+				free(ptr[k]);
+			free(ptr);
 			return (NULL);
 		}
-
-		for (int j = 0; j < width; j++)
-		{
-			grid[i][j] = 0;
-		}
 	}
-
-	return (grid);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			*(ptr[i] + j) = 0;
+	}
+	return (ptr);
 }
 
