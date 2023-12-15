e5f888569766b001dd8d1210bd102dcda2bdc9ab
Code021-raelle <gabrielakinshola3@gmail.com>
2023-12-15 14:57:01 +0100

    dynamic

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Code021-raelle/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
-export LD_PRELOAD=/tmp/hacked.so
+export LD_PRELOAD=/tmp/librand.so
+./gm 9 8 10 24 75 9
+
---
+++ 0x18-dynamic_libraries/rand.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * rand - random numbers to select from
+ *
+ * Return: void
+ */
+int rand(void)
+{
+	return (987654321);
+}
+
